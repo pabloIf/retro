f63ef2779a12441f14ca6280264f42505d376057
Pavlo Saltikov <aknelsaltikov@email.com>
2025-09-14 17:22:47 +0300

    created core models, registered, performed migration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created']
+    list_filter = ['category', 'available', 'created']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+    ordering = ['name']
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=164, db_index=True)
+    slug = models.SlugField(max_length=164, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=164, db_index=True)
+    slug = models.SlugField(max_length=164, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
